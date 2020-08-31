6d5156352928518a0bafd46583a34532ae9095b3
Justin Perez <justin@startrev.com>
2020-08-31 11:12:30 -0700

    Add code to receive message events from Facebook

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const verifyWebhook = require('./verify-webhook');
+const messageWebhook = require('./message-webhook');
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', verifyWebhook);
+app.post('/', messageWebhook);
 
 app.listen(5000, () => console.log('Server is listening on port 5000'));
