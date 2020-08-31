96687f711f9d5d5ab8dc6881e6afedbb1fd19512
Justin Perez <justin@startrev.com>
2020-08-31 03:23:43 -0700

    Setup express server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(5000, () => console.log('Server is listening on port 5000'));
