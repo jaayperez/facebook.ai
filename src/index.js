051c740ea78be73a0a97d34cff84a3721421db13
Justin Perez <justin@startrev.com>
2020-08-31 03:29:05 -0700

    Setup the endpoint that will receive events from the Facebook messenger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const verifyWebhook = require('./verify-webhook');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', verifyWebhook);
 
 app.listen(5000, () => console.log('Server is listening on port 5000'));
